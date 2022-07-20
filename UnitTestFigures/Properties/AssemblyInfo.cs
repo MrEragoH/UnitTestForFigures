0145546f493259c31d1b53cdee599e8523c1fb41
Your Name <you@example.com>
2022-07-20 12:38:08 +0300

    Add project files.

---
+++ UnitTestFigures/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFigures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFigures")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3bd8f1a0-405e-41f6-a1aa-dcf12e97bd1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestFigures/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CircleAndTriangle;
+
+namespace UnitTestFigures
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CorrectData()
+        {
+            string Rez = CircleAndTriangle.Figures.Area(15, 8, 17, 5);
+            string Correct = @"Площадь круга = 78,5
+Площадь треугольника = 60
+Треугольник является прямоугольным";
+            Assert.AreEqual(Rez,Correct);
+        }
+        [TestMethod]
+        public void NegativeSides()
+        {
+            string Rez = CircleAndTriangle.Figures.Area(15, -8, -17, 2);
+            string Correct = @"Площадь круга = 12,56
+Треугольника с заданными сторонами не существует";
+            Assert.AreEqual(Rez, Correct);
+        }
+        [TestMethod]
+        public void NegativeRadius()
+        {
+            string Rez = CircleAndTriangle.Figures.Area(15, 8, 17, -5);
+            string Correct = @"Круга с заданным радиусом не существует
+Площадь треугольника = 60
+Треугольник является прямоугольным";
+            Assert.AreEqual(Rez, Correct);
+        }
+        [TestMethod]
+        public void RadiusAreZero()
+        {
+            string Rez = CircleAndTriangle.Figures.Area(15, 8, 17, 0);
+            string Correct = @"Круга с заданным радиусом не существует
+Площадь треугольника = 60
+Треугольник является прямоугольным";
+            Assert.AreEqual(Rez, Correct);
+        }
+        [TestMethod]
+        public void ZeroOneSides()
+        {
+            string Rez = CircleAndTriangle.Figures.Area(15, 0, 17, 5);
+            string Correct = @"Площадь круга = 78,5
+Треугольника с заданными сторонами не существует";
+            Assert.AreEqual(Rez, Correct);
+        }
+        [TestMethod]
+        public void AllSidesAreZero()
+        {
+            string Rez = CircleAndTriangle.Figures.Area(0, 0, 0, 5);
+            string Correct = @"Площадь круга = 78,5
+Треугольника с заданными сторонами не существует";
+            Assert.AreEqual(Rez, Correct);
+        }
+    }
+}
